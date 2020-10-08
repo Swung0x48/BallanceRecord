0d91240f13bed40b9b73af251f2be3bf4f2ba350
Swung 0x48 <swung0x48@outlook.com>
2020-10-08 11:06:37 +0800

    A fresh ASP.NET Core project (Not working yet)

---
+++ BallanceRecordApi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BallanceRecordApi.Controllers
+{
+    public class TestController: Controller
+    {
+        [HttpGet("api/user")]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "HelloWorld" });
+        }
+    }
+}
---
+++ BallanceRecordApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BallanceRecordApi.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
