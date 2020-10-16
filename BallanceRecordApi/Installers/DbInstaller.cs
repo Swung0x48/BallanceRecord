6a9bc1c2f5e508020f54c3d06a2cd9e842338bcd
Swung0x48 <swung0x48@outlook.com>
2020-10-16 23:34:39 +0800

    Put password options in configuration file.

--- BallanceRecordApi/Installers/DbInstaller.cs
+++ BallanceRecordApi/Installers/DbInstaller.cs
@@ -13,18 +13,23 @@ namespace BallanceRecordApi.Installers
         {
             services.AddDbContext<DataContext>(options =>
                 options.UseMySql(
-                    configuration.GetConnectionString("DefaultConnection")));
+                    configuration.GetConnectionString("DefaultConnection"), 
+                    opt => opt.EnableRetryOnFailure()));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<DataContext>();
             
+            var passwordOptions = new PasswordOptions();
+            configuration.Bind(nameof(passwordOptions), passwordOptions);
+            services.AddSingleton(passwordOptions);
+            
             services.Configure<IdentityOptions>(options =>
             {
-                options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = true;
-                options.Password.RequiredLength = 6;
-                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireDigit = passwordOptions.RequireDigit;
+                options.Password.RequireLowercase = passwordOptions.RequireLowercase;
+                options.Password.RequireNonAlphanumeric = passwordOptions.RequireNonAlphanumeric;
+                options.Password.RequireUppercase = passwordOptions.RequireUppercase;
+                options.Password.RequiredLength = passwordOptions.RequiredLength;
+                options.Password.RequiredUniqueChars = passwordOptions.RequiredUniqueChars;
             });
             
             services.AddScoped<IRecordService, RecordService>();
