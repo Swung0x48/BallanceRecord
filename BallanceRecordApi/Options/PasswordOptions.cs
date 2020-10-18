5647d61dab227f5b9b0aa1121d32af19c2b4c42c
Swung0x48 <swung0x48@outlook.com>
2020-10-18 09:30:42 +0800

    Password schemes no longer hardcoded and now available in appsettings.json

---
+++ BallanceRecordApi/Options/PasswordOptions.cs
@@ -0,0 +1,17 @@
+namespace BallanceRecordApi.Options
+{
+    public class PasswordOptions
+    {
+        public bool RequireDigit { get; set; }
+
+        public bool RequireLowercase { get; set; }
+
+        public bool RequireNonAlphanumeric { get; set; }
+
+        public bool RequireUppercase { get; set; }
+
+        public int RequiredLength { get; set; }
+
+        public int RequiredUniqueChars { get; set; }
+    }
+}
