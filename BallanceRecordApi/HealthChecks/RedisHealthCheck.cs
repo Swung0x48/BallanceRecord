f28639862814b62e669f408ac9b2e5ab5119ae9c
Swung0x48 <swung0x48@outlook.com>
2021-01-16 19:01:05 +0800

    Bug fix: Health check exception thrown when not using redis

--- BallanceRecordApi/HealthChecks/RedisHealthCheck.cs
+++ BallanceRecordApi/HealthChecks/RedisHealthCheck.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using BallanceRecordApi.Cache;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using StackExchange.Redis;
 
@@ -9,14 +10,25 @@ namespace BallanceRecordApi.HealthChecks
     public class RedisHealthCheck: IHealthCheck
     {
         private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private readonly RedisCacheOptions _redisCacheOptions;
 
-        public RedisHealthCheck(IConnectionMultiplexer connectionMultiplexer)
+        public RedisHealthCheck(RedisCacheOptions redisCacheOptions, IConnectionMultiplexer connectionMultiplexer = null)
         {
+            _redisCacheOptions = redisCacheOptions;
+            if (!redisCacheOptions.Enabled)
+            {
+                _connectionMultiplexer = null;
+                return;
+            }
+
             _connectionMultiplexer = connectionMultiplexer;
         }
 
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
         {
+            if (!_redisCacheOptions.Enabled)
+                return await Task.FromResult(HealthCheckResult.Degraded("Redis cache is explicitly disabled."));
+            
             try
             {
                 var database = _connectionMultiplexer.GetDatabase();
