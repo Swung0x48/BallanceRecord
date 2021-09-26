b0f4445022703a18852c9da138601a3e70a44d81
Swung0x48 <swung0x48@outlook.com>
2021-09-26 09:19:33 +0800

    feat: new version db

---
+++ BallanceRecordApi/Data/Migrations_/20210313063302_AddTimeCreatedModified.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BallanceRecordApi.Data.Migrations
+{
+    public partial class AddTimeCreatedModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Records");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "Records",
+                type: "time(6)",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeCreated",
+                table: "Records",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeModified",
+                table: "Records",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Records");
+
+            migrationBuilder.DropColumn(
+                name: "TimeCreated",
+                table: "Records");
+
+            migrationBuilder.DropColumn(
+                name: "TimeModified",
+                table: "Records");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Time",
+                table: "Records",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
---
+++ BallanceRecordApi/Data/Migrations_/20210314053011_AddBallStatus.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BallanceRecordApi.Data.Migrations
+{
+    public partial class AddBallStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "BallSpeed",
+                table: "Records",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBouncing",
+                table: "Records",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BallSpeed",
+                table: "Records");
+
+            migrationBuilder.DropColumn(
+                name: "IsBouncing",
+                table: "Records");
+        }
+    }
+}
