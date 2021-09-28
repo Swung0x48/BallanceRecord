ecfbef22848216546fa159ada2dab11cb9b3ac66
Swung0x48 <swung0x48@outlook.com>
2021-09-28 10:31:35 +0800

    fix: migrations for proper db regeneration

---
+++ BallanceRecordApi/Data/Migrations/20210117031553_AddMapHash.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BallanceRecordApi.Data.Migrations
+{
+    public partial class AddMapHash : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MapHash",
+                table: "Records",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MapHash",
+                table: "Records");
+        }
+    }
+}
---
+++ BallanceRecordApi/Data/Migrations/20210211025751_RenameNameToRemark.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BallanceRecordApi.Data.Migrations
+{
+    public partial class RenameNameToRemark : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Records",
+                newName: "Remark");
+
+            // migrationBuilder.RenameColumn(
+            //     name: "Remark",
+            //     table: "AspNetRoles",
+            //     newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Records",
+                newName: "Remark");
+
+            // migrationBuilder.RenameColumn(
+            //     name: "Name",
+            //     table: "AspNetRoles",
+            //     newName: "Remark");
+        }
+    }
+}
